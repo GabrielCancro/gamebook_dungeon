ba0f5f2fecd4eca367be0723db8808ca88dd50fa
gabrielcancro <gabrielcancro@3ogs.com.ar>
2023-06-01 12:06:45 -0300

    update

--- adventure_core/Fade.gd
+++ adventure_core/Fade.gd
@@ -2,7 +2,6 @@ extends Control
 
 signal end_fade
 
-
 func _ready():
 	visible = true
 
@@ -10,13 +9,13 @@ func fadeIn(time=.3, delay=0):
 	visible = true
 	$Tween.interpolate_property($ColorRect,"modulate",$ColorRect.modulate,Color(1,1,1,1),time,Tween.TRANS_LINEAR,Tween.EASE_IN,delay)
 	$Tween.start()
-	yield($Tween,"tween_completed")
+	yield(get_tree().create_timer(time+delay),"timeout")
 	emit_signal("end_fade")
 
 func fadeOut(time=.3, delay=0):
 	$Tween.interpolate_property($ColorRect,"modulate",$ColorRect.modulate,Color(1,1,1,0),time,Tween.TRANS_LINEAR,Tween.EASE_IN,delay)
 	$Tween.start()
-	yield($Tween,"tween_completed")
+	yield(get_tree().create_timer(time+delay),"timeout")
 	visible = false
 	emit_signal("end_fade")
 
--- scenes/Intro.gd
+++ scenes/Intro.gd
@@ -1,12 +1,5 @@
 extends Control
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	$Button.connect("button_down",self,"onStartClick")
 	$Fade.fadeOut(1.2)
